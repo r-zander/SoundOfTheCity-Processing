5e421a60062e92cc541816519cc400a775d00b73
R. Zander <r-zander@users.noreply.github.com>
2016-08-09 13:22:59 +0000

    Fixed building shapes, included touch events.

--- js/BuildingShape.js
+++ js/BuildingShape.js
@@ -21,30 +21,6 @@ BuildingShape.prototype.getProbability = function () {
     return 1;
 };
 
-BuildingShape.prototype.canBeRotated = function () {
-    return true;
-};
-
 BuildingShape.prototype.newShape = function (width, height) {
-    var shape = this.createShape(width, height);
-
-    if (this.canBeRotated()) {
-        var random = randomInt(0, 3);
-        switch (random) {
-            case 1:
-                shape.translate(width, 0);
-                break;
-            case 2:
-                shape.translate(width, height);
-                break;
-            case 3:
-                shape.translate(0, height);
-                break;
-            default:
-                break;
-        }
-        shape.rotate(random * HALF_PI);
-    }
-
-    return shape;
+    return this.createShape(width, height);
 };
--- js/Shape.js
+++ js/Shape.js
@@ -3,16 +3,31 @@
  * Created by XieLong on 21.07.2016.
  */
 
-function Shape() {
+function Shape(width, height, canBeRotated) {
+    this.width = width;
+    this.height = height;
+    this.canBeRotated = defaultFor(canBeRotated, true);
+
+    if (this.canBeRotated){
+        this.rotationRandom = randomInt(0, 3);
+    }
 }
 
-Shape.prototype.drawAtPosition = function (x, y) {
-    push();
-    translate(x, y);
-    this.draw();
-    pop();
-};
-
-Shape.prototype.draw = function () {
-    throw "Not implemented!";
+Shape.prototype.beforeDraw = function () {
+    if (this.canBeRotated) {
+        switch (this.rotationRandom) {
+            case 1:
+                translate(this.width, 0);
+                break;
+            case 2:
+                translate(this.width, this.height);
+                break;
+            case 3:
+                translate(0, this.height);
+                break;
+            default:
+                break;
+        }
+        rotate(this.rotationRandom * HALF_PI);
+    }
 };
